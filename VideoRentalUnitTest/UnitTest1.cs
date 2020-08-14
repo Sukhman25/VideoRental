26de84af6f09659491d0ec966e1f31b37c58aba0
Sukhmanpreet Singh Romana <54517612+Sukhman25@users.noreply.github.com>
2020-08-14 23:56:25 +1200

    Add files via upload

---
+++ VideoRentalUnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VideoRentalUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DatabaseConnectionTest()
+        {
+            try
+            {
+
+                Database dh = new Database();
+                string result = da.DatabaseConnection();
+                Assert.AreEqual("Connected", result);
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Test Failed: " + ex.ToString());
+                Assert.Fail();
+            }
+        }
+    }
+}
